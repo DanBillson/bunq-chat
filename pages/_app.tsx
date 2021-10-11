979801915a650797fc8e0fde41fcfc51bee387f4
Dan Billson <danny.billson@hotmail.com>
2021-10-11 17:16:08 +0100

    bootstrap app 👞

---
+++ pages/_app.tsx
@@ -0,0 +1,48 @@
+import '@fontsource/montserrat'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { normalize } from 'styled-normalize'
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+    font-family: Montserrat, sans-serif;
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  p {
+    letter-spacing: 0.5px;
+    line-height: 1.6rem;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#00e4bd',
+    secondary: '#9865d5',
+    tertiary: '#fc0064',
+    fg: '#272727',
+    bg: '#fff',
+  },
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>Bunq Chat</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
